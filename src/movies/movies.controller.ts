b52cc790eb9ea802a8886f0828516c09182f7b86
ChanyoungPark59 <126754788+ChanyoungPark59@users.noreply.github.com>
2023-03-10 18:43:14 +0900

    make_create_movie_dto

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -13,29 +14,30 @@ export class MoviesController {
     }
 
     @Get("search")
-    search(@Query('year') searchinhYear: string){
-        return `We are searching for a movie made after: ${searchinhYear}`;
+    search(@Query('year') searchinYear: number){
+        return `We are searching for a movie made after: ${searchinYear}`;
     }
 
     @Get('/:id')
-    getOne(@Param('id') movieId: string): Movie {
+    getOne(@Param('id') movieId: number): Movie {
+        console.log(typeof movieId);
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDto){
         return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
-    remove(@Param('id') movieId: string){
+    remove(@Param('id') movieId: number){
         return this.moviesService.deleteOne(movieId);
     }
 
     // @Put 모든 것을 업데이트 하여서 patch를 주로 이용
 
     @Patch('/:id')
-    patch(@Param('id') movieId: string, @Body() updateData){
+    patch(@Param('id') movieId: number, @Body() updateData){
         return this.moviesService.update(movieId, updateData);
     }
 
